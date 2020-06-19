8f5a736c2e71e48f90f9ba51ffe50a5c6342855c
SaeWoo <nams@nist.gov>
2020-06-19 08:29:21 -0600

    add if statement to switch between adapters

--- list_et.py
+++ list_et.py
@@ -7,17 +7,15 @@ count_uuid = '292bd3d2-14ff-45ed-9343-55d125edb721'
 rw_uuid = '56cd7757-5f47-4dcd-a787-07d648956068'
 data_uuid = 'fec26ec4-6d71-4442-9f81-55bc21d658d6'
 
-print("Trying to find bluetooth adapter")
-adapter = pygatt.GATTToolBackend()
-adapter.start()
-devices = adapter.scan(run_as_root=True, timeout=3  )
 name = sys.argv[1]
-
+print("Trying to find bluetooth adapter")
 if False:
-    #adapter = pygatt.BGAPIBackend()
-    print("initialize BT adapter")
+    adapter = pygatt.GATTToolBackend()
     adapter.start()
-    print("Trying to find NIST ET devices")
+    devices = adapter.scan(run_as_root=True, timeout=3  )
+else:
+    adapter = pygatt.BGAPIBackend()
+    adapter.start()
     devices = adapter.scan(run_as_root=True, timeout=3  )
 # devices = adapter.scan(timeout=10  )
 et_devices = []
